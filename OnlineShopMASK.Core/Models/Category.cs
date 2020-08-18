273e1bb475e2c7497302790e0ed6d8680a2d3316
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-08-18 14:55:21 +0200

    updating category and adding decoratores

--- OnlineShopMASK.Core/Models/Category.cs
+++ OnlineShopMASK.Core/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,13 @@ namespace OnlineShopMASK.Core.Models
     public class Category
     {
         public string Id { get; set; }
+
+        [Display(Name = "Category Name")]
+        [Required]
+        [StringLength(100)]
         public string  CategoryName { get; set; }
+        [Display(Name = "Description")]
+        [StringLength(600)]
         public string Description { get; set; }
         public Category()
         {
