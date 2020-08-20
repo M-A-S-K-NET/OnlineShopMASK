ec6793e3b3de9a1a22f1dc879b2836156ef91caf
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2020-08-20 12:42:07 +0200

    Updated the Delete method in sql repository

--- OnlineShopMASK.DataAccess.SQL/SqlRepository.cs
+++ OnlineShopMASK.DataAccess.SQL/SqlRepository.cs
@@ -35,10 +35,10 @@ namespace OnlineShopMASK.DataAccess.SQL
         {
             var t = Find(Id);
             if (context.Entry(t).State == EntityState.Detached)
-            {
+            
                 dbSet.Attach(t);
                 dbSet.Remove(t);
-            }
+            
         }
 
         public T Find(string Id)
