2209b2ab1ec0872dbbd7a8373ab86afd80776193
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2020-08-20 10:35:06 +0200

    Footer Layout Done

--- OnlineShopMASK.WebUI/Controllers/HomeController.cs
+++ OnlineShopMASK.WebUI/Controllers/HomeController.cs
@@ -15,15 +15,43 @@ namespace OnlineShopMASK.WebUI.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "About Mask Online Shop";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Information";
+            return View();
+        }
 
+        public ActionResult Conditions()
+        {
+            ViewBag.Message = "Terms & Conditions ";
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+            ViewBag.Message = "Privacy Policy ";
+            return View();
+        }
+
+        public ActionResult OurServices()
+        {
+            ViewBag.Message = "Our Services ";
+            return View();
+        }
+
+        public ActionResult Warranty()
+        {
+            ViewBag.Message = "Warranty & Repair ";
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            ViewBag.Message = "Frequently Asked Questions ";
             return View();
         }
     }
