0317f2c689da60c182daabc17f1baea42ad7ad6f
Sowjanya-Penmetsa <56627133+Sowjanya-Penmetsa@users.noreply.github.com>
2020-08-26 10:47:32 +0200

    Contact view page in footer is designed

--- OnlineShopMASK.WebUI/App_Start/BundleConfig.cs
+++ OnlineShopMASK.WebUI/App_Start/BundleConfig.cs
@@ -25,9 +25,7 @@ namespace OnlineShopMASK.WebUI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include
-                ("~/Scripts/umd/popper.js",
-                "~/Scripts/bootstrap.js"));
+           
         }
 
     }
